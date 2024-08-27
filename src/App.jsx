bd1faedfee06236eb34dbee1ffe0a76901ae1399
25-manoj <manojwebdevelop@gmail.com>
2024-08-27 16:05:47 +0530

    game logic implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Cell from "./Components/Cell"
 
 function App() {
@@ -7,12 +7,50 @@ function App() {
   const [winningMessage,setWinningMessage] = useState(null)
 
   const message = 'it is now '+ go + "'s go"
+
+  const checkScore = () =>{
+    const winningCombo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      winningCombo.forEach(array => {
+        let circleWins = array.every(cell=>cells[cell]==='circle')
+        if(circleWins){
+          setWinningMessage('Circle Wins')
+          return
+        }})
+        winningCombo.forEach(array => {
+          let crossWins = array.every(cell=>cells[cell]==='cross')
+          if(crossWins){
+            setWinningMessage('Cross Wins')
+            return
+          }})
+}
+
+useEffect(()=>{
+  checkScore()
+},[cells])
+
   return (
   <div className="app">
     <h1>Tic Tac Toe</h1>
     <div className="gameboard">
       {cells.map((cell,index)=>
-        <Cell cell={cell} key={index} id={index} go={go} setGo={setGo}/>
+        <Cell 
+        cell={cell} 
+        key={index} 
+        id={index} 
+        go={go} 
+        setGo={setGo} 
+        setCells={setCells} 
+        cells={cells}
+        winningMessage={winningMessage}/>
       )}
     </div>
     <p className="info">{winningMessage || message}</p>
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,22 +1,38 @@
-const Cell = ({id,cell,go,setGo}) =>{
+const Cell = ({id,cell,go,setGo,setCells,cells,winningMessage}) =>{
     function handleClick(e){
+    if(!winningMessage){
       const taken = e.target.firstChild.classList.contains('circle') || e.target.firstChild.classList.contains('cross')
       if(!taken){
         if(go === 'circle'){
             e.target.firstChild.classList.add('circle')
+            handleCellChange('circle')
             setGo('cross')
         }
         if(go === 'cross'){
             e.target.firstChild.classList.add('cross')
+            handleCellChange('cross')
             setGo('circle')
         }
       }
-    }
+    }}
+const handleCellChange = (className) => {
+    const nextCells = cells.map((cell,index)=>{
+        if(index === id){
+            return className
+        } else {
+            return cell
+        }
+    })
+    setCells(nextCells)
+}
+
+
+
     return (
         <div className="square" id={id} onClick={handleClick}>
-            <div>{cell}</div>
+            <div className={cell}></div>
         </div>
     )
 }
 
-export default Cell
+export default Cell 
